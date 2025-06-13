e27340cbc7540ccd1abe5577ef25891c9876c805
huetruong <truongvshieu01@gmail.com>
2025-06-13 07:32:27 +0700

    fix

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
